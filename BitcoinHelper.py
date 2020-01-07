6a56ed3d40e9042ba9992ba07e3d2a03a6c631fb
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2020-01-07 11:30:25 -0500

    I AM PROUD TO ANNOUNCE THAT I GOT THE PRICE OF 1 BTC. YOU MAY CELEBRATE WITH AUTH TOKEN GENERATOR!

--- BitcoinHelper.py
+++ BitcoinHelper.py
@@ -1,25 +1,41 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-import re
-def get_access():
+
+
+def getPrice():
+    KEY = getAuthKey()
+    USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0'
+    HEADERS = {'User-Agent': USER_AGENT, 'Origin': 'https://robinhood.com', 'Referer': 'https://robinhood.com/',
+               'Host': 'api.robinhood.com', 'Authorization': KEY, 'Accept-Encoding': 'gzip,deflate,br',
+               'TE': 'Trailers'}
+
+    response = requests.get("https://api.robinhood.com/marketdata/forex/quotes/3d961844-d360-45fc-989b-f6fca761d511/", headers=HEADERS)
+    responseJSON = json.loads(response.content)
+    return responseJSON['ask_price']
+
+
+def scrapeWeb():
     url = 'https://robinhood.com/crypto/BTC'
     req = requests.get(url)
-    m = re.search('\swindow.auth\s*=\s*(.+);', req.text)
-    data = json.loads(m.group(1))
-    return '{0} {1}'.format(data['token_type'], data['access_token'])
-def get_price(head):
-    urldomain = 'https://api.robinhood.com/marketdata/forex/quotes/3d961844-d360-45fc-989b-f6fca761d511/'
-    request = requests.get(urldomain, headers=head)
-    info = request.json()
-    price = info.get('mark_price')
-    return float(price)
-token = get_access()
-price = get_price({'authorization': token})
-print(price)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    return soup
+
+
+def getAuthKey():
+    js_script = (scrapeWeb().select('script')[0]).text
+
+    split_js = js_script.split('\n')
+    winAuth = (split_js[28].split(":"))[1]
+
+    # removes 13 characters from the end and strips unnecessary quotation marks
+    winAuthToken = winAuth[:-13].strip('"')
+    return "Bearer " + winAuthToken
 
 
 # returns JSON given file path and name
 def getJSON(filePathAndName):
     with open(filePathAndName, 'r') as fp:
         return json.load(fp)
+
+print(getPrice())
