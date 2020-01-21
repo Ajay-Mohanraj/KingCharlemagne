e5f1f758a82abcf9368140337dfe8c438db3ac88
Gary Zlobinskiy <garzlo22@bergen.org>
2020-01-20 21:08:00 -0500

    styled shell output

--- BitcoinHelper.py
+++ BitcoinHelper.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-#hi
 def getPrice():
     KEY = getAuthKey()
     USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0'
